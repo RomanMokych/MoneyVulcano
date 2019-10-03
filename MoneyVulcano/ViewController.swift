060646cce841e7396d4fcac09ef28d1653434dbf
Roman Mokych <mokych.roman@gmail.com>
2019-10-03 18:33:23 +0300

    Initial Commit

---
+++ MoneyVulcano/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoneyVulcano
+//
+//  Created by Roman Mokych on 10/3/19.
+//  Copyright © 2019 Roman Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
