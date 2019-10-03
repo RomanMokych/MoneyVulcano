16fb3c2a705255ce290a67229010448b2d7dbe6d
Roman Mokych <mokych.roman@gmail.com>
2019-10-03 19:36:33 +0300

    Implemented base UI + added app icons

--- MoneyVulcano/GoalViewController.swift
+++ MoneyVulcano/GoalViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  GoalViewController.swift
 //  MoneyVulcano
 //
 //  Created by Roman Mokych on 10/3/19.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
